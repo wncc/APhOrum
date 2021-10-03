8594c06b07eb4f3fa558b5253635aa5b5cb949f0
unstablebrainiac <sajalnarang@gmail.com>
2021-10-03 15:48:26 +0530

    init server.go

---
+++ backend/server.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+    "log"
+    "net/http"
+)
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+    if r.URL.Path != "/hello" {
+        http.Error(w, "404 not found.", http.StatusNotFound)
+        return
+    }
+
+    if r.Method != "GET" {
+        http.Error(w, "Method is not supported.", http.StatusNotFound)
+        return
+    }
+
+    fmt.Fprintf(w, "Hello World!")
+}
+
+func main() {
+	fileServer := http.FileServer(http.Dir("./static"))
+    http.Handle("/", fileServer)
+    http.HandleFunc("/hello", helloHandler)
+
+    fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+        log.Fatal(err)
+    }
+}
