5c09f7c94d5e6ac5c3f3c5a2e50cf77919a945c6
Kumar Ayush <cheekujodhpur@gmail.com>
2021-12-15 20:01:06 +0530

    Polish bulletin css

--- frontend/src/Bulletin.js
+++ frontend/src/Bulletin.js
@@ -6,6 +6,7 @@ import Box from '@mui/material/Box'
 import Button from '@mui/material/Button'
 import Card from '@mui/material/Card'
 import CardContent from '@mui/material/CardContent'
+import Grid from '@mui/material/Grid'
 import TextField from '@mui/material/TextField'
 import Typography from '@mui/material/Typography'
 
@@ -25,9 +26,13 @@ class Bulletin extends Component {
     }).then(response => {
       if (response.ok) return response.json()
     }).then(json => {
-      json.forEach(message => {
-        messages.push(message)
-      })
+      if (json === null)
+        messages.push("No messages")
+      else {
+        json.forEach(message => {
+          messages.push(message.Message)
+        })
+      }
       this.setState({ messages: messages }) // trigger rerender
     }).catch(err => {
       console.log('Error:', err);
@@ -49,7 +54,6 @@ class Bulletin extends Component {
     }).then(response => {
       if (response.ok) return response.json()
     }).then(json => {
-      console.log('Output:', json);
       this.setState({ m: '' })
       this.updateMessage();
     }).catch(err => {
@@ -59,25 +63,37 @@ class Bulletin extends Component {
 
   render() {
     return (
-      <Box>
-        <Box component="form" novalidate autoComplete="off" onSubmit={this.handleSubmit}>
-          <TextField onChange={(event) => this.setState({ m: event.target.value })} value={this.state.m} id="apho_message" label="Bulletin Message" variant="outlined" />
-          <Button variant="contained" type="submit">Submit</Button>
-        </Box>
-        <Box>
-          {this.state.messages.map(message => {
-            return (
-              <Card>
-                <CardContent>
-                  <Typography variant="body2">
-                    {message.Message}
-                  </Typography>
-                </CardContent>
-              </Card>
-            )
-          })}
-        </Box>
-      </Box>
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <Box component="form" novalidate autoComplete="off" onSubmit={this.handleSubmit}>
+            <Typography variant="h4" sx={{ mb: 2 }}>Send a new message</Typography>
+            <TextField
+              multiline
+              fullWidth
+              onChange={(event) => this.setState({ m: event.target.value })} value={this.state.m}
+              label="What is the message?"
+              variant="outlined"
+            />
+            <Box sx={{ pt: 2 }}><Button variant="contained" type="submit">Submit</Button></Box>
+          </Box>
+        </Grid>
+        <Grid item xs={8}>
+          <Box>
+            <Typography variant="h4" sx={{ mb: 2 }}>Current messages</Typography>
+            {this.state.messages.map(message => {
+              return (
+                <Card sx={{ my: 1 }}>
+                  <CardContent>
+                    <Typography variant="h6">
+                      {message}
+                    </Typography>
+                  </CardContent>
+                </Card>
+              )
+            })}
+          </Box>
+        </Grid>
+      </Grid>
     )
   };
 }
